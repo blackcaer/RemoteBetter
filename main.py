413cbee4e04b8b565fd335a208282661a1588a10
blackcaer <kryspin123@onet.pl>
2024-04-13 16:14:43 +0200

    Added test function for development in TEST_MODE Fixed waiting before executing Loading key to global variable Added fallbacks for config

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import configparser
+import os
 import sys
 import time
 from datetime import datetime, timedelta
@@ -11,6 +12,7 @@ from ItemRustDatabase import ItemRustDatabase
 from SteamTradeHandler import SteamTradeHandler
 from fernet_wrapper import Wrapper
 from seleniumbase import SB
+from selenium.webdriver.remote.webelement import WebElement
 
 from RemoteBetter import RemoteBetter
 
@@ -43,6 +45,50 @@ async def update_rustitems(remote_better):
     remote_better.inventory = list(filter(lambda x: x.all_success, remote_better.inventory))
     print("Items minus failures: ", len(remote_better.inventory))
 
+async def test(remote_better):
+    ".pt-intent-danger"
+    r = remote_better
+    r.load_rch_cookies(TOKEN_FILEPATH)
+    r.sb.driver.uc_open("https://rustchance.com/easter-event")
+    btns = r.sb.find_elements(".event-button.event-case__open")
+    print(r.sb.is_element_present(".pt-intent-danger"))
+    #print(".event-button.event-case__open")
+    1==1
+
+    print(r.sb.is_element_present(".supplydrops-button"))
+    spl=r.sb.find_element(".supplydrops-button")
+    spl.click()
+    await asyncio.sleep(6)
+    print(r.sb.is_element_in_an_iframe(".ctp-checkbox-label"))
+    r.sb.switch_to_frame(0)  # Przełącz na pierwszy iframe na stronie
+    r.sb.click(".ctp-checkbox-label")  # Kliknij przycisk z klasą .ctp-checkbox-label
+
+    print(r.sb.is_element_present(".pt-intent-danger"))
+
+
+    await asyncio.sleep(20)
+    btns[0].click()
+    await asyncio.sleep(1)
+    btns[0].click()
+    await asyncio.sleep(1)
+    btns[0].click()
+
+    print(r.sb.is_element_present(".pt-intent-danger"))
+    #await asyncio.sleep(8)
+
+    warns = r.sb.find_elements(".pt-intent-danger")
+    warn = warns[0]
+
+    for warn in warns:
+        warn:WebElement
+        print(warn.text)
+
+    print("slep")
+    await asyncio.sleep(10)
+
+
+
+    return
 
 async def get_inventory(remote_better):
     if LOAD_INV:
@@ -82,20 +128,14 @@ async def body():
     min_tax_frac = 0.02
     max_tax_frac = 0.05
 
-    if not LOAD_PASS:  # for debugging
-        key = Wrapper.key_from_pass(password=input("Provide password: "))
-    else:
-        with open("testpass.txt", 'r') as file:
-            key = Wrapper.key_from_pass(file.read())
-            print("PASS FROM FILE USED")
-
-    # print("WAITING 10 MIN, DELETE THAT")
-    # await asyncio.sleep(600)
-
     while True:
         with SB(demo=False, uc=True, uc_cdp_events=True, uc_cdp=True, test=TEST_MODE, headless=HEADLESS) as sb:
-
             r = RemoteBetter(sb)
+
+            if TEST_MODE:
+                await test(r)
+                print("sleeping 40ss")
+                await asyncio.sleep(40)
 
             # Scrape inventory from site
             await get_inventory(r)
@@ -113,7 +153,7 @@ async def body():
             # Accept trade (to site)
             try:
                 await asyncio.sleep(5)
-                await accept_offers(key, gifts_only=False)
+                await accept_offers(global_key, gifts_only=False)
             except Exception as e:
                 print("Trade accept error: " + str(e))
             print("Waiting 60sec...")
@@ -122,7 +162,7 @@ async def body():
             print("Next bet at ~", time_after(11830))
 
             try:
-                await accept_offers(key, gifts_only=False)
+                await accept_offers(global_key, gifts_only=False)
             except Exception as e:
                 print("Trade accept error: " + str(e))
             print("Waiting 110sec...")
@@ -130,7 +170,7 @@ async def body():
             await asyncio.sleep(110)
 
             try:
-                await accept_offers(key, gifts_only=True)
+                await accept_offers(global_key, gifts_only=True)
             except Exception as e:
                 print("Trade accept error: " + str(e))
             print("Waiting 710sec... (and then even longer)")
@@ -140,7 +180,7 @@ async def body():
             _ = asyncio.create_task(ItemRust.database.save_database_async())  # Not awaited because there's no need to
             for i in range(11):
                 await asyncio.sleep(1000)
-                print(f"Sleep {(i + 1) * 100}/11000 sec passed")
+                print(f"Sleep {(i + 1) * 1000}/11000 sec passed")
         print("Wyszlo z petli, super")
 
 
@@ -165,16 +205,16 @@ def read_config(filename):
 
     DATA_DICT = {
         'wotanex': {
-            'TOKEN_FILEPATH': config.get('Global', 'TOKEN_FILEPATH_wotanex'),
-            'ACC_FILE': config.get('Global', 'ACC_FILE_wotanex'),
-            'minprice': config.getfloat('Global', 'minprice_wotanex'),
-            'maxprice': config.getfloat('Global', 'maxprice_wotanex')
+            'TOKEN_FILEPATH': config.get('Global', 'TOKEN_FILEPATH_wotanex',fallback=None),
+            'ACC_FILE': config.get('Global', 'ACC_FILE_wotanex',fallback=None),
+            'minprice': config.getfloat('Global', 'minprice_wotanex',fallback=None),
+            'maxprice': config.getfloat('Global', 'maxprice_wotanex',fallback=None)
         },
         'dark': {
-            'TOKEN_FILEPATH': config.get('Global', 'TOKEN_FILEPATH_dark'),
-            'ACC_FILE': config.get('Global', 'ACC_FILE_dark'),
-            'minprice': config.getfloat('Global', 'minprice_dark'),
-            'maxprice': config.getfloat('Global', 'maxprice_dark')
+            'TOKEN_FILEPATH': config.get('Global', 'TOKEN_FILEPATH_dark',fallback=None),
+            'ACC_FILE': config.get('Global', 'ACC_FILE_dark',fallback=None),
+            'minprice': config.getfloat('Global', 'minprice_dark',fallback=None),
+            'maxprice': config.getfloat('Global', 'maxprice_dark',fallback=None)
         }
     }
 
@@ -182,7 +222,7 @@ def read_config(filename):
 def _pre_start_operations():
     read_config('config.txt')
 
-    global TOKEN_FILEPATH, ACC_FILE, minprice, maxprice
+    global TOKEN_FILEPATH, ACC_FILE, minprice, maxprice, global_key
     try:
         data = DATA_DICT[MODE]
         TOKEN_FILEPATH = data["TOKEN_FILEPATH"]
@@ -212,6 +252,13 @@ def _pre_start_operations():
         input("Press key to continue")
         input("Press key to continue")
 
+    if not LOAD_PASS:  # for debugging
+        global_key = Wrapper.key_from_pass(password=input("Provide password: "))
+        os.system('cls')
+    else:
+        with open("testpass.txt", 'r') as file:
+            global_key = Wrapper.key_from_pass(file.read())
+            print("PASS FROM FILE USED")
 
 async def main():
     # Loading data:
